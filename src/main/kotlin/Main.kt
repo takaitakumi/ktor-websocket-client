3584997be00d59143f923e2c5e0e2e7d27c7b1ae
tomgao <376801941@qq.com>
2022-12-17 17:57:55 +0900

    改进聊天系统 客户端发送消息的时候不影响接受消息

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,8 @@ import io.ktor.client.*
 import io.ktor.client.plugins.websocket.*
 import io.ktor.http.*
 import io.ktor.websocket.*
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main() {
@@ -12,15 +14,39 @@ fun main() {
     runBlocking {
         client.webSocket(method = HttpMethod.Get, host = "127.0.0.1", port = 8080, path = "/chat") {
             while (true) {
-                val othersMessage = incoming.receive() as? Frame.Text ?: continue
-                println(othersMessage.readText())
-                val myMessage = readLine()
-                if (myMessage != null) {
-                    send(myMessage)
-                }
+                val messageOutputRoutine = launch { outputMessages() }
+                val userInputRoutine = launch { inputMessages() }
+
+                userInputRoutine.join()
+                messageOutputRoutine.cancelAndJoin()
             }
         }
     }
     client.close()
     println("Connection closed. Goodbye!")
+}
+
+// figure out what is suspend fun
+suspend fun DefaultClientWebSocketSession.outputMessages() {
+    try {
+        for (message in incoming) {
+            message as? Frame.Text ?: continue
+            println(message.readText())
+        }
+    } catch (e: Exception) {
+        println("Error while receiving: " + e.localizedMessage)
+    }
+}
+
+suspend fun DefaultClientWebSocketSession.inputMessages() {
+    while (true) {
+        val message = readLine() ?: ""
+        if (message.equals("exit", true)) return
+        try {
+            send(message)
+        } catch (e: Exception) {
+            println("Error while sending: " + e.localizedMessage)
+            return
+        }
+    }
 }
